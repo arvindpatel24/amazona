19285aa8e790337798c5bb25a965be812906cdc8
arvindpatel24 <arvind.patel@juspay.in>
2022-12-03 00:21:18 +0530

    First look

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="">amazona</a>
+      </header>
+      <main>List Product</main>
+    </div>
+  );
+}
+
+export default App;
