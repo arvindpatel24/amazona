7481175a14a3cde86e4e0ba535576c085351755b
arvindpatel24 <arvind.patel@juspay.in>
2022-12-18 10:55:29 +0530

    moved data to mongojs

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 import express from "express";
 import data from "./data.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+
+dotenv.config();
+console.log(process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to DB');
+}).catch(err => {
+    console.log("Error : ", err.message);
+})
 
 
 const app = express();
+app.use('/api/seed', seedRouter);
+app.use('/api/product', productRouter);
 
 var corsOptions = {
   origin: 'http://localhost:3000',
@@ -14,25 +29,6 @@ app.get("/", (req,res) => {
     res.send("Server is up and running...");
 })
 
-app.get("/api/product", (req,res) => {
-    res.send(data.products);
-})
-
-app.get("/api/product/slug/:slug", (req,res) => {
-    const product = data.products.find(pro => pro.slug === req.params.slug);
-    if(product)
-        res.send(product);
-    else
-        res.status(404).send({message: "Product not found"});
-})
-
-app.get("/api/product/:id", (req,res) => {
-    const product = data.products.find(pro => pro._id === req.params.id);
-    if(product)
-        res.send(product);
-    else
-        res.status(404).send({message: "Product not found"});
-})
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
